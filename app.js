89252909a998b26d03a9bf5302865f955a1f3181
mauris <mauri.schults@khk.ee>
2020-11-11 18:41:02 +0200

    Add task to list function

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-const form = document.querySelector("#task-form")
-const taskInput = document.querySelector("#task");
-const taskList = document.querySelector(".collection");
-form.addEventListener("submit", addTask);
+// define variables
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('.collection');
+// define event listeners
+// add task to list - submit button
+form.addEventListener('submit', addTask);
 
+// addTask function
 function addTask(e) {
-    if (taskInput.value === "") {
-        alert("Add new task")
+    if (taskInput.value === '') {
+        alert("Add new task!")
     } else {
-        const li = document.createElement("Li");
-        li.className = "collection-item";
+        // create li
+        const li = document.createElement('li');
+        // add class
+        li.className = 'collection-item';
+        // add input value
         li.appendChild(document.createTextNode(taskInput.value));
-        const link = document.createElement("a");
-        link.className = "secondary-content";
-        link.innerHTML = '<i class="fas fa-backspace"></i>'
+        // link for element removing
+        const link = document.createElement('a');
+        // add css to link
+        link.className = 'secondary-content';
+        // add fas icon
+        link.innerHTML = '<i class="fas fa-backspace"></i>';
+        // add link into li
         li.appendChild(link);
+        // add li into ul
         taskList.appendChild(li);
-        taskInput.value = "";
+        // clear task input
+        taskInput.value = '';
         e.preventDefault();
     }
 }
